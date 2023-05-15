6c6ee7e81212d00e3f16b60bc36e86639b6c1798
datsfilipe <datsfilipe.foss@proton.me>
2023-05-14 21:36:16 -0300

    test: replace babel for @swc/jest for jest (update coverage)

--- jest.config.ts
+++ jest.config.ts
@@ -7,5 +7,20 @@ export default {
   "moduleNameMapper":{
     "\\.(css|less|sass|scss)$": "<rootDir>/__mocks__/styleMock.js",
     "\\.(gif|ttf|eot|svg)$": "<rootDir>/__mocks__/fileMock.js"
-  }
+  },
+  transform: {
+    "^.+\\.(t|j)sx?$": ["@swc/jest", {
+      "jsc": {
+        "parser": {
+          "tsx": true,
+          "syntax": "typescript",
+        },
+        "transform": {
+          "react": {
+            "runtime": "automatic"
+          }
+        },
+      },
+    }],
+  },
 };
